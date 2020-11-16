f11d1b0860bc04ebaed0118b6c7094ef3e0aedf3
David Marques <mouradavid8@gmail.com>
2020-11-16 18:19:46 -0300

    feat(error): Starting error module

--- src/errors/entities/error.entity.ts
+++ src/errors/entities/error.entity.ts
@@ -1,1 +1,12 @@
-export class Error {}
+import { ResponseDto } from "src/utils/dto/response.dto"
+import { Domain } from "./domain-erro.entity"
+import { TypeError } from "./type-error.entity"
+
+export class Error {
+    id: number
+    dateTime: Date
+    type: TypeError
+    domain: Domain
+    message: string
+    data: ResponseDto
+}
